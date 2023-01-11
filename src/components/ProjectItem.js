391f17680e45231f382c7ebe065d8c7c60b3b06c
Randy Kipkurui Cherutich <randy.kipkurui4@gmail.com>
2023-01-11 23:07:23 +0300

    Completed Lists and Keys Lab

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,18 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  function TechItem(props) {
+    return <span>{props.technologies}</span>;
+  }
+  const techie = technologies.map((tech) =>{
+    return <TechItem key={tech} technologies={tech}/>;
+  })
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {techie}
       </div>
     </div>
   );
